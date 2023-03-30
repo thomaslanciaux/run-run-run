98cae1ae89fdb992003dcfad9ffee8343b9c6d42
Thomas Lanciaux <thomaslanciaux@Thomass-MacBook-Pro.local>
2023-03-30 18:11:33 +0200

    Change page title

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,8 +15,8 @@ const CryptoRunner = () => {
   return (
     <>
       <Head>
-        <title>Crypto run</title>
-        <meta name="description" content="Crypto run" />
+        <title>Run run run</title>
+        <meta name="description" content="Run run run" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
