83f409413828c9bc39e537f1bf8b0461985e6bb6
Thomas Lanciaux <thomaslanciaux@Thomass-MacBook-Pro.local>
2023-04-02 21:08:54 +0200

    Test env

--- src/components/world.js
+++ src/components/world.js
@@ -58,20 +58,28 @@ const World = ({ player }) => {
         shadow-mapSize-width={2048}
         shadow-bias={-0.0001}
       />
-      <group position={[0, -0.5, 0]} receiveShadow>
+      <group position={[6, 0.25, 0]} receiveShadow>
         {[...Array(FLOOR_ITEMS)].map((_value, index) => (
           <MovingFloorItem
             key={index}
             position={[0, 0, -OFFSET + (index / FLOOR_ITEMS) * OFFSET * 2]}
             player={player}
           >
-            <mesh receiveShadow>
-              <boxGeometry args={[4, 1, 3]} />
+            <mesh receiveShadow castShadow>
+              <boxGeometry args={[0.5, 0.5, 2]} />
+              <meshLambertMaterial />
+            </mesh>
+            <mesh receiveShadow castShadow position={[-10, 0, 0]}>
+              <boxGeometry args={[0.5, 0.5, 2]} />
               <meshLambertMaterial />
             </mesh>
           </MovingFloorItem>
         ))}
       </group>
+      <mesh rotation-x={-Math.PI/2} receiveShadow position={[0, 0, 0]}>
+        <planeGeometry args={[15, 120]} />
+        <shadowMaterial opacity={0.1} />
+      </mesh>
       <group position={[3, 0, 0]}>
         <Cloud position={[0, 5, -5]} args={[1, 1]} scale={0.4} />
         <Cloud position={[0, 6, -7]} args={[1, 2]} scale={0.2} />
