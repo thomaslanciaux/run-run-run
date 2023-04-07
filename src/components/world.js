4c299eaf5d5771c1039b888f19040d21fc70a068
Thomas Lanciaux <thomaslanciaux@Thomass-MacBook-Pro.local>
2023-04-07 19:29:15 +0200

    Improve shadows

--- src/components/world.js
+++ src/components/world.js
@@ -37,7 +37,7 @@ const World = () => {
         shadow-camera-top={30}
         shadow-camera-left={-30}
         shadow-camera-right={30}
-        shadow-mapSize={1024}
+        shadow-mapSize={2048}
         shadow-bias={-0.00001}
         intensity={0.5}
       />
@@ -61,8 +61,8 @@ const World = () => {
         <shadowMaterial opacity={0.1} />
       </mesh>
       <group position={[3, 0, 0]}>
-        <Cloud texture={CLOUD_TEXTURE} depth={1} position={[10, 5, -5]} args={[1, 1]} scale={0.4} />
-        <Cloud texture={CLOUD_TEXTURE} depth={5} position={[10, 6, -7]} args={[1, 2]} scale={0.2} />
+        <Cloud texture={CLOUD_TEXTURE} depth={1} position={[10, 10, -5]} args={[1, 1]} scale={1} />
+        <Cloud texture={CLOUD_TEXTURE} depth={5} position={[10, 8, -7]} args={[1, 2]} scale={0.8} />
         <Cloud texture={CLOUD_TEXTURE} depth={3} position={[10, 5, -7]} args={[1, 2]} scale={0.2} />
         <Cloud texture={CLOUD_TEXTURE} depth={2} position={[10, 10, 20]} args={[1, 3]} scale={0.8} />
       </group>
