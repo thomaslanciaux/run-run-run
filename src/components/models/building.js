c5878c91f2b0ebd98fc2e291e7d15171c82ec4e0
Thomas Lanciaux <thomaslanciaux@Thomass-MacBook-Pro.local>
2023-04-22 08:58:04 +0200

    Improve building graphics

--- src/components/models/building.js
+++ src/components/models/building.js
@@ -7,13 +7,19 @@ const Building = (props) => {
   return (
     <group {...props} dispose={null} ref={ref}>
       <mesh castShadow receiveShadow geometry={nodes.Box15819_Meat_0_1.geometry}>
-        <meshStandardMaterial color={props.color} />
+        <meshStandardMaterial color={props.color} metalness="0.5" roughness="0.5" />
       </mesh>
       <mesh castShadow receiveShadow geometry={nodes.Box15819_Meat_0_2.geometry} material={materials.brown} />
-      <mesh castShadow receiveShadow geometry={nodes.Box15819_Meat_0_3.geometry} material={materials.mirror} />
-      <mesh castShadow receiveShadow geometry={nodes.Box15819_Meat_0_4.geometry} material={materials.concret2} />
+      <mesh castShadow receiveShadow geometry={nodes.Box15819_Meat_0_3.geometry}>
+        <meshStandardMaterial metalness="1" roughness="0.2" />
+      </mesh>
+      <mesh castShadow receiveShadow geometry={nodes.Box15819_Meat_0_4.geometry}>
+        <meshStandardMaterial color="#666666" />
+      </mesh>
       <mesh castShadow receiveShadow geometry={nodes.Box15819_Meat_0_5.geometry} material={materials.metal} />
-      <mesh castShadow receiveShadow geometry={nodes.Box15819_Meat_0_6.geometry} material={materials.concret} />
+      <mesh castShadow receiveShadow geometry={nodes.Box15819_Meat_0_6.geometry}>
+        <meshStandardMaterial color="#666666" />
+      </mesh>
       <mesh castShadow receiveShadow geometry={nodes.Box15819_Meat_0_7.geometry} material={materials.L_sign} />
       <mesh castShadow receiveShadow geometry={nodes.Box15819_Meat_0_8.geometry} material={materials.dark} />
     </group>
