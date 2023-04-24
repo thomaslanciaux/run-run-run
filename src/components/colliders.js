a7756f522495e0634f275c46845299f2fa5e2690
Thomas Lanciaux <thomaslanciaux@Thomass-MacBook-Pro.local>
2023-04-24 19:01:58 +0200

    Add another obstacle model instance

--- src/components/colliders.js
+++ src/components/colliders.js
@@ -2,30 +2,36 @@ import MovingItem from '@/components/moving-item';
 import { Instances, Hydrant } from '@/components/models/hydrant';
 import { BinInstances, Bin } from '@/components/models/bin';
 import { MailboxInstances, Mailbox } from '@/components/models/mailbox';
+import { ElectricBoxInstances, ElectricBox } from '@/components/models/box';
 
 const Colliders = ({ setColliders, obstacles }) => {
   return (
     <Instances>
       <BinInstances>
         <MailboxInstances>
-          {obstacles.map(({ positionZ, type }, index) => (
-            <MovingItem
-              key={index}
-              setColliders={setColliders}
-              position={[0, 0.5, positionZ]}
-              offset={(obstacles[obstacles.length - 1].positionZ / 2) + 1}
-            >
-              {type === 0 && (
-                <Hydrant scale={0.3} position={[0, -.5, 0]} rotation-y={Math.PI} />
-              )}
-              {type === 1 && (
-                <Bin scale={0.0065} position={[0, -.5, 0]} />
-              )}
-              {type === 2 && (
-                <Mailbox scale={[0.6, 0.4, 0.4]} position={[0, -.6, 0]} rotation-y={Math.PI / 2} />
-              )}
-            </MovingItem>
-          ))}
+          <ElectricBoxInstances>
+            {obstacles.map(({ positionZ, type }, index) => (
+              <MovingItem
+                key={index}
+                setColliders={setColliders}
+                position={[0, 0.5, positionZ]}
+                offset={(obstacles[obstacles.length - 1].positionZ / 2) + 1}
+              >
+                {type === 0 && (
+                  <Hydrant scale={0.3} position={[0, -0.5, 0]} />
+                )}
+                {type === 1 && (
+                  <Bin scale={0.0065} position={[0, -0.5, 0]} />
+                )}
+                {type === 2 && (
+                  <Mailbox scale={[0.6, 0.4, 0.4]} position={[0, -0.6, 0]} rotation-y={Math.PI / 2} />
+                )}
+                {type === 3 && (
+                  <ElectricBox scale={0.085} position={[0, -0.6, 0]} rotation-y={-Math.PI / 2}/>
+                )}
+              </MovingItem>
+            ))}
+          </ElectricBoxInstances>
         </MailboxInstances>
       </BinInstances>
     </Instances>
