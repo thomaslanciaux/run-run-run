e1a17d63c94104d0c4c17699e35c96a329fa3f14
Kev <numerized@users.noreply.github.com>
2023-09-24 00:09:35 +0200

    fix

--- src/components/game.js
+++ src/components/game.js
@@ -1,30 +1,54 @@
-import { useEffect, Suspense, useRef } from 'react';
-import { useGameContext } from '@/hooks/game-context';
-import { Canvas } from '@react-three/fiber';
-import { AdaptiveDpr, AdaptiveEvents, Stats, Loader } from '@react-three/drei';
-import World from '@/components/world';
-import Player from '@/components/player';
-import Score from '@/components/score';
-import StartScreen from '@/components/start-screen';
-import GameoverScreen from '@/components/gameover-screen';
-import PausedScreen from '@/components/paused-screen';
-import Colliders from '@/components/colliders';
-import CheckColliders from '@/components/check-colliders';
-import { generateObstacles } from '@/libs/utils';
+import { SuppressLongpressGesture } from "capacitor-suppress-longpress-gesture";
+SuppressLongpressGesture.activateService();
+import { useEffect, Suspense, useRef } from "react";
+import { useGameContext } from "@/hooks/game-context";
+import { Canvas } from "@react-three/fiber";
+import { AdaptiveDpr, AdaptiveEvents, Stats, Loader } from "@react-three/drei";
+import World from "@/components/world";
+import Player from "@/components/player";
+import Score from "@/components/score";
+import StartScreen from "@/components/start-screen";
+import GameoverScreen from "@/components/gameover-screen";
+import PausedScreen from "@/components/paused-screen";
+import Colliders from "@/components/colliders";
+import CheckColliders from "@/components/check-colliders";
+import { generateObstacles } from "@/libs/utils";
 
 const obstacles = generateObstacles();
 
+const createHandler = (func, timeout) => {
+  let timer = null;
+  let pressed = false;
+
+  return function () {
+    if (timer) {
+      clearTimeout(timer);
+    }
+
+    if (pressed) {
+      if (func) {
+        func.apply(this, arguments);
+      }
+      clear();
+    } else {
+      pressed = true;
+      setTimeout(clear, timeout || 500);
+    }
+  };
+
+  function clear() {
+    timeout = null;
+    pressed = false;
+  }
+};
+
 const Game = () => {
-  const {
-    setIsPaused,
-    setIsPlaying,
-    setGameOver,
-    colliders,
-  } = useGameContext();
+  const { setIsPaused, setIsPlaying, setGameOver, colliders } =
+    useGameContext();
 
   const score = useRef(0);
   const acceleration = useRef(0);
-  const player = useRef(null)
+  const player = useRef(null);
 
   const resetGame = () => {
     score.current = 0;
@@ -39,16 +63,20 @@ const Game = () => {
   };
 
   useEffect(() => {
-    window.addEventListener('blur', () => setIsPaused(true));
+    const ignore = createHandler((e) => e.preventDefault(), 500);
+    document.body.addEventListener("touchstart", ignore, { passive: false });
+    window.addEventListener("blur", () => setIsPaused(true));
     return () => {
-      window.removeEventListener('blur', () => setIsPaused(true));
+      window.removeEventListener("blur", () => setIsPaused(true));
     };
   });
 
   return (
-    <div className={`
+    <div
+      className={`
       flex h-full items-center justify-center bg-gradient-to-b from-blue-400 to-blue-500
-    `}>
+    `}
+    >
       <PausedScreen />
       <GameoverScreen resetGame={resetGame} score={score} />
       <StartScreen resetGame={resetGame} player={player} />
@@ -57,7 +85,7 @@ const Game = () => {
         camera={{
           position: [-12, 3, -6],
           fov: 50,
-          far: 300
+          far: 300,
         }}
       >
         <Suspense fallback={null}>
--- src/components/models/building2.js
+++ src/components/models/building2.js
@@ -1,48 +1,80 @@
-import React, { useRef, useMemo, useContext, createContext } from 'react'
-import { useGLTF, Merged } from '@react-three/drei'
+import React, { useRef, useMemo, useContext, createContext } from "react";
+import { useGLTF, Merged } from "@react-three/drei";
 
-const context = createContext()
+const context = createContext();
 
 export function Building2Instances({ children, ...props }) {
-  const { nodes } = useGLTF('/models/building2.glb')
+  const { nodes } = useGLTF("/models/building2.glb");
   const instances = useMemo(
     () => ({
-      ObjectDefault: nodes['Object014_15_-_Default6_0'],
-      ObjectDefault1: nodes['Object015_17_-_Default_0'],
-      ObjectDefaultghj: nodes['Object016_16_-_Defaultghj_0'],
-      ObjectDefault2: nodes['Object017_17_-_Default_0'],
-      ObjectDefault3: nodes['Object018_06_-_Default_0'],
+      ObjectDefault: nodes["Object014_15_-_Default6_0"],
+      ObjectDefault1: nodes["Object015_17_-_Default_0"],
+      ObjectDefaultghj: nodes["Object016_16_-_Defaultghj_0"],
+      ObjectDefault2: nodes["Object017_17_-_Default_0"],
+      ObjectDefault3: nodes["Object018_06_-_Default_0"],
       Object: nodes.Object019_04_0,
-      ObjectDefault4: nodes['Object020_15_-_Default_0'],
+      ObjectDefault4: nodes["Object020_15_-_Default_0"],
     }),
     [nodes]
-  )
+  );
   return (
     <Merged meshes={instances} {...props} receiveShadow castShadow>
-      {(instances) => <context.Provider value={instances} children={children} />}
+      {(instances) => (
+        <context.Provider value={instances}>
+          {children}
+        </context.Provider>
+      )}
     </Merged>
-  )
+  );
 }
 
 export function Building2(props) {
-  const instances = useContext(context)
+  const instances = useContext(context);
   return (
     <group {...props} dispose={null}>
       <group rotation={[-Math.PI / 2, 0, 0]} scale={0.59}>
         <group rotation={[Math.PI / 2, 0, 0]}>
-          <group position={[-1.49, 55.77, 60.12]} rotation={[-Math.PI / 2, 0, Math.PI / 2]} scale={0.63}>
+          <group
+            position={[-1.49, 55.77, 60.12]}
+            rotation={[-Math.PI / 2, 0, Math.PI / 2]}
+            scale={0.63}
+          >
             <instances.ObjectDefault1 position={[77, 0.43, -88.68]} />
           </group>
-          <instances.ObjectDefault position={[-1.76, 0, 11.7]} rotation={[-Math.PI / 2, 0, Math.PI / 2]} scale={0.63} />
-          <instances.ObjectDefaultghj position={[-1.76, 0, 11.7]} rotation={[-Math.PI / 2, 0, Math.PI / 2]} scale={0.63} />
-          <instances.ObjectDefault2 position={[-1.76, 0, 11.7]} rotation={[-Math.PI / 2, 0, Math.PI / 2]} scale={0.63} />
-          <instances.ObjectDefault3 position={[-1.76, 0, 11.7]} rotation={[-Math.PI / 2, 0, Math.PI / 2]} scale={0.63} />
-          <instances.Object position={[-1.76, 0, 11.7]} rotation={[-Math.PI / 2, 0, Math.PI / 2]} scale={0.63} />
-          <instances.ObjectDefault4 position={[-1.76, 0, 11.7]} rotation={[-Math.PI / 2, 0, Math.PI / 2]} scale={0.63} />
+          <instances.ObjectDefault
+            position={[-1.76, 0, 11.7]}
+            rotation={[-Math.PI / 2, 0, Math.PI / 2]}
+            scale={0.63}
+          />
+          <instances.ObjectDefaultghj
+            position={[-1.76, 0, 11.7]}
+            rotation={[-Math.PI / 2, 0, Math.PI / 2]}
+            scale={0.63}
+          />
+          <instances.ObjectDefault2
+            position={[-1.76, 0, 11.7]}
+            rotation={[-Math.PI / 2, 0, Math.PI / 2]}
+            scale={0.63}
+          />
+          <instances.ObjectDefault3
+            position={[-1.76, 0, 11.7]}
+            rotation={[-Math.PI / 2, 0, Math.PI / 2]}
+            scale={0.63}
+          />
+          <instances.Object
+            position={[-1.76, 0, 11.7]}
+            rotation={[-Math.PI / 2, 0, Math.PI / 2]}
+            scale={0.63}
+          />
+          <instances.ObjectDefault4
+            position={[-1.76, 0, 11.7]}
+            rotation={[-Math.PI / 2, 0, Math.PI / 2]}
+            scale={0.63}
+          />
         </group>
       </group>
     </group>
-  )
+  );
 }
 
-useGLTF.preload('/models/building2.glb')
+useGLTF.preload("/models/building2.glb");
